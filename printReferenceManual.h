2fdf7726e17f0e7f883b7174ab9a5fba9937ef60
Sabareesh_Kanagaraj <34646057+SabareeshKanagaraj87@users.noreply.github.com>
2022-01-12 00:31:06 +0530

    Update printReferenceManual.h

--- printReferenceManual.h
+++ printReferenceManual.h
@@ -1,9 +1,11 @@
+#include "GetColorFromPairNumber.h"
+
 void printReferenceManual()
 {
   
   for (int i=0;i<5;++i)
   {
-  
+    GetColorFromPairNumber(i)
     printf("Pair No : %d - Major Color : %s, Minor Color : %s",)
   }
   
