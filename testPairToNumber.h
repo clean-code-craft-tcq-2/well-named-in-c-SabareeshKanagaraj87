3eb8017f340c5b2fc81b966ad59557a1394fda38
Sabareesh_Kanagaraj <34646057+SabareeshKanagaraj87@users.noreply.github.com>
2022-01-11 20:46:06 +0530

    Update testPairToNumber.h

--- testPairToNumber.h
+++ testPairToNumber.h
@@ -1,1 +1,12 @@
-#testPairToNumber
+void testPairToNumber(
+    enum MajorColor major,
+    enum MinorColor minor,
+    int expectedPairNumber)
+{
+    ColorPair colorPair;
+    colorPair.majorColor = major;
+    colorPair.minorColor = minor;
+    int pairNumber = GetPairNumberFromColor(&colorPair);
+    printf("Got pair number %d\n", pairNumber);
+    assert(pairNumber == expectedPairNumber);
+}
