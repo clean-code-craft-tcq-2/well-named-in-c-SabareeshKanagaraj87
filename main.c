d6e2fd39975080c2afb608004aed67bc46138a63
Sabareesh_Kanagaraj <34646057+SabareeshKanagaraj87@users.noreply.github.com>
2022-01-11 21:42:27 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,9 +3,6 @@
 #include "testNumberToPair.h"
 #include "testPairToNumber.h"
 
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
 int main() {
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
