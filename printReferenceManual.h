c0c3ccb1a86e94365abb29959769174f1388331f
Sabareesh_Kanagaraj <34646057+SabareeshKanagaraj87@users.noreply.github.com>
2022-01-12 00:34:42 +0530

    Update printReferenceManual.h

--- printReferenceManual.h
+++ printReferenceManual.h
@@ -1,12 +1,18 @@
 #include "GetColorFromPairNumber.h"
+#include "ColorPairToString.h"
 
 void printReferenceManual()
 {
   
   for (int i=0;i<5;++i)
   {
-    GetColorFromPairNumber(i)
-    printf("Pair No : %d - Major Color : %s, Minor Color : %s",)
+    ColorPair colorPair = GetColorFromPairNumber(i);
+    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+    ColorPairToString(&colorPair, colorPairNames);
+    #printf("Got pair %s\n", colorPairNames);
+    assert(colorPair.majorColor == expectedMajor);
+    assert(colorPair.minorColor == expectedMinor);
+    printf("Pair No : %d - Major Color : %s, Minor Color : %s\n",i,colorPair.majorColor,colorPair.minorColor)
   }
   
 }
