bb53236be9c90ac608f82a523e52de1612d918bc
Sabareesh_Kanagaraj <34646057+SabareeshKanagaraj87@users.noreply.github.com>
2022-01-12 00:57:53 +0530

    Update printReferenceManual.h

--- printReferenceManual.h
+++ printReferenceManual.h
@@ -4,7 +4,8 @@
 void printReferenceManual()
 {
   int i ;
-  for (i=1;i<MAX_COLORPAIR_NAME_CHARS;i++)
+  int numberOfColorCode = numberOfMajorColors * numberOfMinorColors;
+  for (i=1;i<numberOfColorCode;i++)
   {
     ColorPair colorPair = GetColorFromPairNumber(i);
     char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
